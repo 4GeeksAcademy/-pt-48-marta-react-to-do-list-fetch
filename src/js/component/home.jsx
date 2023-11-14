5507ee0e986d2f4f4442c42323a45d74818b7353
Marta Mohedano <139046036+mmohedano@users.noreply.github.com>
2023-11-14 23:36:23 +0000

    api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,26 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 let nextId = 0;
 
 const Home = () => {
 	const [thing, setThing] = useState('');
 	const [pends, setPends] = useState([]);
-  
+
+	const getInfoAPI = async () => {
+		const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/martaml',{
+			method:'GET'
+		} 
+		);
+		const data = await response.json();
+
+		setPends(data);
+	}
+    
+	useEffect (() => {
+		getInfoAPI()
+
+	}, []);
+
 	return (
 	  <> <div className="List__container">
 		<h1>To do list:</h1>
@@ -17,13 +32,13 @@ const Home = () => {
 		<button className="button-add" onClick={() => {
 		  setPends([
 			...pends,
-			{ id: nextId++, thing: thing}
+			{ id: nextId++, label: thing}
 		  ]);
 		}}>Add</button>
 		</div>
 		<ul>
 		  {pends.map(pend => (
-			<li key={pend.id}>{pend.thing}{' '}
+			<li key={pend.id}>{pend.label}{' '}
 			<button className="button-delete" onClick={() => {
               setPends(
                 pends.filter(a =>
